102631c663c546d242b70e931582d5bae0847db0
Justin Hager <coachjustinhager@gmail.com>
2021-02-10 13:32:12 -0800

    adds morgan and error handler

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 const peopleRouter = require('../people/people-router');
 const petsRouter = require('../pets/pets-router');
 
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(function(req, res, next) {
+    // Solution from: https://www.positronx.io/express-cors-tutorial/
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  app.
 app.use(express.json());
 app.use('/api/people', peopleRouter);
 app.use('/api/pets', petsRouter);
@@ -22,5 +33,14 @@ app.use('/', (req,res) => {
 		.status(200)
 		.send(msg);
 });
+
+app.use(function errorHandler(error, req, res, next) {
+    let response;
+  
+    response = { error: { message: 'server error' } };
+    console.error(error);
+  
+    res.status(500).json(response);
+  });
 
 module.exports = app;
